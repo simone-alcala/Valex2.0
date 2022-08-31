fe9b003a7427a2ebf71370004d20f95912ea9338
simone-alcala <simone.alcala@gmail.com>
2022-08-31 00:08:24 -0300

    Fix: database conection

--- src/database.ts
+++ src/database.ts
@@ -6,16 +6,15 @@ dotenv.config();
 const { Pool } = pg;
 
 const databaseConfig = { 
-  connectionString: process.env.DATABASE_URL 
+  connectionString: process.env.DATABASE_URL,
+  ssl: null
 };
 
-/*
 if (process.env.MODE === 'PROD') {
   databaseConfig.ssl = {
     rejectUnauthorized: false
   }
 }
-*/
 
 export const connection = new Pool(databaseConfig);
 
